ed0ebbafc55d30fccf0db42c3171c8ded0f1cf77
arindambckv <arin.psb@rediffmail.com>
2025-05-28 22:04:41 +0530

    updated

---
+++ RDSML Day 10 Scatter Plot Part I/scatter.R
@@ -0,0 +1,18 @@
+#install.packages("ggplot2")
+library(ggplot2)
+dataset = mtcars
+
+#generating basic scatter plot
+ggplot(dataset, aes(x = wt, y = mpg)) + 
+  geom_point()
+
+#changing the point size and shape
+ggplot(dataset, aes(x = wt, y = mpg)) + 
+  geom_point(size= 2)
+
+ggplot(dataset, aes(x = wt, y = mpg)) + 
+  geom_point(size=4)
+ggplot(dataset, aes(x = wt, y = mpg)) + 
+  geom_point(size=2, shape=1)
+ggplot(dataset, aes(x = wt, y = mpg)) + 
+  geom_point(size=2, shape=5)
